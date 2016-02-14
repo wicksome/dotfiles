a6223f72923fe1d9d312af9c89694990cd62036b
yeongjun <yeongjun@yj.local>
2016-02-15 01:34:47 +0900

    fixed install.sh
    add env backup script

--- install.sh
+++ install.sh
@@ -1,14 +1,47 @@
+#!/bin/bash
 
+# variablea--------------------------------
+# file
+f_bashrc=".bashrc"
+f_bash_profile=".bash_profile"
+f_gitconfig=".gitconfig"
+f_vimrc=".vimrc"
 
-FILE_BASHRC="~/.bashrc"
-FILE_BASH_PROFILE="~/.bash_profile"
-FILE_GITCONFIG="~/.gitconfig"
-FILE_VIMRC="~/.vimrc"
+# source path
+s_bashrc="./$f_bashrc"
+s_bash_profile="./$f_bash_profile"
+s_gitconfig="./$f_gitconfig"
+s_vimrc="./$f_vimrc"
 
+# destination path
+d_bashrc="$HOME/$f_bashrc"
+d_bash_profile="$HOME/$f_bash_profile"
+d_gitconfig="$HOME/$f_gitconfig"
+d_vimrc="$HOME/$f_vimrc"
 
+# etc
+dir_backup_base="$HOME/.myenv"
+dir_backup="$dir_backup_base/$(date +'%Y%m%d-%H%M%S')"
 
-cp -v ./.bashrc $FILE_BASHRC
-cp -v ./.bash_profile ~/.bash_profile
-cp -v ./.gitconfig ~/.gitconfig
-cp -v ./.vimrc ~/.vimrc
+# logic ----------------------------------
+# create base backup directory
+if [ ! -d "$dir_backup_base" ]; then
+	echo "[INFO] CREATE .myenv"
+	mkdir $dir_backup_base
+fi
+
+# backup current env file
+echo "[INFO] CREATE DIR FOR ENV BACKUP: $dir_backup"
+mkdir $dir_backup
+cp $d_bashrc		$dir_backup/$f_bashrc
+cp $d_bash_profile	$dir_backup/$f_bash_profile
+cp $d_gitconfig		$dir_backup/$f_gitconfig
+cp $d_vimrc			$dir_backup/$f_vimrc
+
+# copy to local
+cp $s_bashrc		$d_bashrc
+cp $s_bash_profile	$d_bash_profile
+cp $s_gitconfig		$d_gitconfig
+cp $s_vimrc			$d_vimrc
+
 source ~/.bash_profile
