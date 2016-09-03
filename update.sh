ae8b9bee85f4ee2446c3d8761169c959e671359e
yeongjun.kim <opid911@gmail.com>
2016-09-04 03:46:00 +0900

    updated update.sh

--- update.sh
+++ update.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
 # 현재 환경설정 파일로 갱신하기
-FILE_BASHRC="$HOME/.bashrc"
-FILE_BASH_PROFILE="$HOME/.bash_profile"
-FILE_GITCONFIG="$HOME/.gitconfig"
-FILE_VIMRC="$HOME/.vimrc"
-FILE_ATOM_KEYMAP="$HOME/.atom/keymap.cson"
 
-cp -v $FILE_BASHRC ./.bashrc
-cp -v $FILE_BASH_PROFILE ./.bash_profile
-cp -v $FILE_GITCONFIG ./.gitconfig
-cp -v $FILE_VIMRC ./.vimrc
-cp -v $FILE_ATOM_KEYMAP ./keymap.cson
+# file
+f_bashrc=".bashrc"
+f_bash_profile=".bash_profile"
+f_gitconfig=".gitconfig"
+f_vimrc=".vimrc"
+f_atom_keymap="keymap.cson"
+
+# target path
+tp_bashrc="$HOME/$f_bashrc"
+tp_bash_profile="$HOME/$f_bash_profile"
+tp_gitconfig="$HOME/$f_gitconfig"
+tp_vimrc="$HOME/$f_vimrc"
+tp_atom_keymap="$HOME/.atom/$f_atom_keymap"
+
+# source path
+sp_bashrc="./$f_bashrc"
+sp_bash_profile="./$f_bash_profile"
+sp_gitconfig="./$f_gitconfig"
+sp_vimrc="./$f_vimrc"
+sp_atom_keymap="./$f_atom_keymap"
+
+cp -v $tp_bashrc $sp_bashrc
+cp -v $tp_bash_profile $sp_bash_profile
+cp -v $tp_gitconfig $sp_gitconfig
+cp -v $tp_vimrc $sp_vimrc
+cp -v $tp_atom_keymap $sp_atom_keymap
