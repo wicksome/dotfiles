f0c304df1b0d9f18616ff8043ea866d0eb17286b
yeongjun.kim <opid911@gmail.com>
2017-12-11 11:55:35 +0900

    Update brew-cask.sh

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -7,35 +7,30 @@ brew tap caskroom/cask
 brew tap caskroom/versions
 
 # Install packages
-
-apps=(
-  caskroom/versions/java8
-  appcleaner
-  atom
-  bartender
-  bettertouchtool
-  cheatsheet
-  dash2
-  dropbox
-  duet
-  evernote
-  fantastical
-  firefox
-  fliqlo
-  glimmerblocker
-  google-chrome
-  google-chrome-canary
-  keycastr
-  hammerspoon
-  macdown
-  popclip
-  slack
-  sublime-text
-  vlc
-  now
-)
-
-brew cask install "${apps[@]}"
+brew cask install caskroom/versions/java8
+brew cask install appcleaner
+brew cask install atom
+brew cask install bartender
+brew cask install bettertouchtool
+brew cask install cheatsheet
+brew cask install dash2
+brew cask install dropbox
+brew cask install duet
+brew cask install evernote
+brew cask install fantastical
+brew cask install firefox
+brew cask install fliqlo
+brew cask install glimmerblocker
+brew cask install google-chrome
+brew cask install google-chrome-canary
+brew cask install keycastr
+brew cask install hammerspoon
+brew cask install macdown
+brew cask install popclip
+brew cask install slack
+brew cask install sublime-text
+brew cask install vlc
+brew cask install now
 
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
