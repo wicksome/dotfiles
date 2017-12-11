80268fb98f11dd5524b7ab77f96b9de1c815aaea
yeongjun.kim <opid911@gmail.com>
2017-12-11 16:25:17 +0900

    Update brew-cask.sh

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -1,36 +1,52 @@
-if ! is-executable brew; then
-  echo "Skipping: Homebrew-Cask (not found: brew)"
-  return
+#!/bin/sh
+
+echo Install all AppStore Apps at first!
+# If you are looking into a solution to automate AppStore installs,
+# check mas https://github.com/argon/mas
+# I installed XCode, Simplenote, Clocks, Stuffit Expander, Cloudapp, Forklift.
+read -p "Press any key to continue... " -n1 -s
+echo  '\n'
+
+# Install Homebrew
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
 fi
 
 brew tap caskroom/cask
 brew tap caskroom/versions
 
-# Install packages
+# Development
 brew cask install caskroom/versions/java8
-brew cask install appcleaner
-brew cask install atom
-brew cask install bartender
-brew cask install bettertouchtool
-brew cask install cheatsheet
-brew cask install dash2
-brew cask install dropbox
-brew cask install duet
-brew cask install evernote
-brew cask install fantastical
-brew cask install firefox
-brew cask install fliqlo
-brew cask install glimmerblocker
-brew cask install google-chrome
-brew cask install google-chrome-canary
-brew cask install keycastr
-brew cask install hammerspoon
-brew cask install macdown
-brew cask install popclip
-brew cask install slack
-brew cask install sublime-text
-brew cask install vlc
-brew cask install now
+
+# Software
+brew cask install --appdir="/Applications" appcleaner
+brew cask install --appdir="/Applications" atom
+brew cask install --appdir="/Applications" bartender
+brew cask install --appdir="/Applications" bettertouchtool
+brew cask install --appdir="/Applications" cheatsheet
+brew cask install --appdir="/Applications" dash2
+brew cask install --appdir="/Applications" dropbox
+brew cask install --appdir="/Applications" duet
+brew cask install --appdir="/Applications" evernote
+brew cask install --appdir="/Applications" fantastical
+brew cask install --appdir="/Applications" firefox
+brew cask install --appdir="/Applications" fliqlo
+brew cask install --appdir="/Applications" glimmerblocker
+brew cask install --appdir="/Applications" google-chrome
+brew cask install --appdir="/Applications" google-chrome-canary
+brew cask install --appdir="/Applications" keycastr
+brew cask install --appdir="/Applications" hammerspoon
+brew cask install --appdir="/Applications" macdown
+brew cask install --appdir="/Applications" popclip
+brew cask install --appdir="/Applications" slack
+brew cask install --appdir="/Applications" sublime-text
+brew cask install --appdir="/Applications" vlc
+brew cask install --appdir="/Applications" now
+brew cask install --appdir="/Applications" 1password
 
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
