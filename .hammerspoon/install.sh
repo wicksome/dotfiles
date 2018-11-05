6f05af2d0ec692f6f85db7f77ab79b4d38e3a796
yeongjun <yeongjun@yeongjun-imac.local>
2018-11-05 18:33:43 +0900

    Update install script of hammerspoon

--- .hammerspoon/install.sh
+++ .hammerspoon/install.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
-echo Install Hammerspoon!
-read -p "Press any key to continue..." -n1 -s
-echo '\n'
 
-if test ! $(which brew)
-then
+echo "Install Hammerspoon!"
+read -p "Press any key to continue..." -n1 -s
+echo ""
+
+if test ! $(which brew); then
   echo "Instaling Homebrew for you."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew tap cashroom/cask
+if ! brew info cask &>/dev/null; then
+  echo "Instaling Homebrew Cask."
+  brew tap cashroom/cask
+fi
+
+echo "Instaling Hammerspoon."
 brew cask install --appdir="/Applications" hammerspoon
 
-cd $(dirname $BASH_SOURCE)
-BASE=$(dirname $(pwd))
-[ -d ~/.hammerspoon ] || ln -sfv "$BASE/.hammerspoon/" ~
+#
+if brew cask ls --versions hammerspoon > /dev/null; then
+  cd $(dirname $BASH_SOURCE)
+  BASE=$(dirname $(pwd))
+  ln -sfv "$BASE/.hammerspoon/" ~
+fi
+
