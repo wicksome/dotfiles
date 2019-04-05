48c114ee91f74dc383c1273d5bb2bff0736640ae
yeongjun.kim <opid911@gmail.com>
2019-04-05 12:52:29 +0900

    update zsh script

--- install/zsh.sh
+++ install/zsh.sh
@@ -1,12 +1,11 @@
 # Install zsh
 brew install zsh
+
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-
 # install powerlever9
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
 
 # install font 
 brew tap caskroom/fonts
@@ -16,6 +15,7 @@ brew cask install font-hack-nerd-font
 # and, Use a different font for non-ASCII text
 # iTerm2 -> Preferences -> Profiles -> Text -> Font -> Change Font
 
+# install zsh plugins
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
