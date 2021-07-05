586d73d4665b95432a63721f51282b1e3b979382
yeongjun.kim <5036939+wicksome@users.noreply.github.com>
2021-07-05 21:38:54 +0900

    Update brew.sh

--- install/brew.sh
+++ install/brew.sh
@@ -25,36 +25,45 @@ brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed
 
-# mac backup
-# brew install mackup
-
 # Install VPN.
-brew install openconnect
-brew install gpg
-brew install pass
+# brew install openconnect
+# brew install gpg
+# brew install pass
 
-# Install other useful binaries.
-# brew install tmux
-# brew install tmuxinator-completion
 brew install wget
-brew install the_silver_searcher # https://github.com/ggreer/the_silver_searcher
 brew install fzf
 brew install autojump   
 brew install git
-brew install git-flow
 brew install git-lfs
 brew install gist
 brew install htop
 brew install tree
 brew install jq
 brew install python-yq
-brew install chrome-cli
 
 # brew install plantuml
 # brew install asciinema
 # brew install asciidoctor
 # brew install asciidoc
 # brew install pandoc
+
+# Check for Homebrew Cask
+if ! brew info cask &>/dev/null; then
+  echo "Instaling Homebrew Cask."
+  brew tap cashroom/cask
+fi
+
+# Development
+brew install jenv
+
+# Applications
+brew cask install --appdir="/Applications" appcleaner
+brew cask install --appdir="/Applications" bartender
+brew cask install --appdir="/Applications" bettertouchtool
+brew cask install --appdir="/Applications" hammerspoon
+brew cask install --appdir="/Applications" visual-studio-code
+brew cask install --appdir="/Applications" vlc
+brew cask install --appdir="/Applications" 1password
 
 # Remove outdated versions from the cellar.
 brew cleanup
